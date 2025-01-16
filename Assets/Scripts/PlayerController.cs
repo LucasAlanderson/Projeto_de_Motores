b50b1f63db9a7730428139a379bb9f016b57b3e0
Alanderson Lucas Silva de Souza <20231170150056@ifrn.local>
2025-01-16 12:03:27 -0300

    feat: adding all files and directories to the repository

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private bool isJumping = false;
+
+    [SerializeField]
+    private float Speed = 4f;
+
+    [SerializeField]
+    private float Jump = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.A)){
+            rb.AddForce(new Vector2(-Speed * 15, transform.position.y));
+        }else if(Input.GetKey(KeyCode.D)){
+            rb.AddForce(new Vector2(Speed * 15, transform.position.y));
+        }else if(Input.GetKey(KeyCode.W) && !isJumping){
+            rb.AddForce(new Vector2(transform.position.x, Jump * 100), ForceMode2D.Force);
+            isJumping = true;
+        }else if(((Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))) && !isJumping){
+            rb.AddForce(new Vector2(-Speed * 15, Jump * 100), ForceMode2D.Force);
+            isJumping = true;
+        }else if(((Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))) && !isJumping){
+            rb.AddForce(new Vector2(Speed * 15, Jump * 100), ForceMode2D.Impulse);
+            isJumping = true;
+        }else if(Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.X)){
+            rb.AddForce(new Vector2(Speed * 100, transform.position.y));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D c)
+    {
+        if(c.gameObject.tag == "ground")
+        {
+            isJumping = false;
+        }
+    }
+}
