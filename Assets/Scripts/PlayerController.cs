42f7f0c8a38276655cf5347ce4d122485738d918
Alanderson Lucas Silva de Souza <20231170150056@ifrn.local>
2025-01-22 17:02:47 -0300

    feat and fix: adding new features and fixing bugs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -14,6 +16,16 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     private float Jump = 10f;
+    [SerializeField]
+    private bool canDash = false;
+    [SerializeField]
+    private float dashCooldown = 2f;
+    [SerializeField]
+    private float dashDuration = Time.deltaTime;
+
+    private float originalGravity;
+
+    private float originalMass;
 
     void Start()
     {
@@ -22,29 +34,51 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.A)){
-            rb.AddForce(new Vector2(-Speed * 15, transform.position.y));
-        }else if(Input.GetKey(KeyCode.D)){
-            rb.AddForce(new Vector2(Speed * 15, transform.position.y));
-        }else if(Input.GetKey(KeyCode.W) && !isJumping){
+        originalGravity = rb.gravityScale;
+        originalMass = rb.mass;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(new Vector2(-Speed * 20f, transform.position.y), ForceMode2D.Force);
+        }else if (Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKey(KeyCode.B))
+            {
+                StartCoroutine(dash());
+            }
+            rb.AddForce(new Vector2(Speed * 20f, transform.position.y), ForceMode2D.Force);
+        }else if (Input.GetKey(KeyCode.Space) && !isJumping)
+        {
             rb.AddForce(new Vector2(transform.position.x, Jump * 130), ForceMode2D.Force);
             isJumping = true;
-        }else if(Input.GetKey(KeyCode.Q) && !isJumping){
+        }
+        else if (Input.GetKey(KeyCode.Q) && !isJumping)
+        {
             rb.AddForce(new Vector2(-Speed * 90, Jump * 130), ForceMode2D.Force);
             isJumping = true;
-        }else if(Input.GetKey(KeyCode.E) && !isJumping){
+        }
+        else if (Input.GetKey(KeyCode.E) && !isJumping)
+        {
             rb.AddForce(new Vector2(Speed * 90, Jump * 130), ForceMode2D.Force);
             isJumping = true;
-        }else if(Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.X)){
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
             rb.AddForce(new Vector2(Speed * 100, transform.position.y), ForceMode2D.Impulse);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D c)
     {
-        if(c.gameObject.tag == "ground")
+        if (c.gameObject.tag == "ground")
         {
             isJumping = false;
         }
+    }
+
+    IEnumerator dash()
+    {
+        
+        yield return null;
     }
 }
